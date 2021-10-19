fa8b0b1994e5b3915474b87be3557510fd99521e
Kavichelvan P <p.kavichelvan@gmail.com>
2021-10-19 14:29:36 +0530

    initial commit

---
+++ src/App.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/kavi/i);
+  expect(linkElement).toBeInTheDocument();
+});
